dd99fc4db61f7df227a8d48c62127e160e5c666c
Đạt Nguyễn <nguyentuandat.vn@gmail.com>
2016-02-17 15:00:53 +0700

    init plugin

---
+++ testcase.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Test Case
+Plugin URI: http://apide.com/
+Description: A connector with freshbooks.com. Help our to create, update invoice and clients,...
+Version: 1.0
+Author: Đạt Nguyễn
+Author URI: http://apide.com/wordpress-plugins/
+License: GPLv2 or later
+Text Domain: apide
+*/
+?>
